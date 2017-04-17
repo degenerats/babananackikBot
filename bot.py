e922b210aa15f2dd59103585dd7c93a7126006c5
naawha <naawha@gmail.com>
2017-04-17 23:15:01 +0500

    Добавил прокси

--- bot.py
+++ bot.py
@@ -1,20 +1,33 @@
-# -*- coding: utf-8 -*-
+# -*- coding:utf-8 -*-
+import telepot
 import config
-import telebot
 import time
+import urllib3
 import re
 
-def listener(messages):
-    for m in messages:
-        if m.content_type == 'text':
-            human = re.findall(r'челик', m.text)
-            dotka = re.findall(r'дотку', m.text)
-            if human:
-                bot.send_message(m.chat.id, 'членик')
-            if dotka:
-                bot.send_message(m.chat.id, 'ебаться в сракотан собрались чтоле?')
+proxy_url = "http://proxy.server:3128"
+telepot.api._pools = {
+    'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3, maxsize=10, retries=False, timeout=30),
+}
+telepot.api._onetime_pool_spec = (urllib3.ProxyManager, dict(proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30))
+bot = telepot.Bot(config.key)
 
-if __name__ == '__main__':
-     bot = telebot.TeleBot(config.token)
-     bot.set_update_listener(listener)
-     bot.polling(none_stop=True)
+def handle(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    print(msg['text'])
+
+    if content_type == 'text':
+        human = re.findall(ur'челик', msg['text'])
+        dotka = re.findall(ur'дотку', msg['text'])
+        if human:
+            bot.sendMessage(chat_id, u'членик')
+        if dotka:
+            bot.sendMessage(chat_id, u'ебаться в сракотан собрались чтоле?')
+
+bot.message_loop(handle)
+
+print ('Listening ...')
+
+# Keep the program running.
+while 1:
+    time.sleep(10)
